8152b666e30273a8dc60ef0189b2588e699e94be
Daniel Jimenez <danieldavidjp@gmail.com>
2021-07-24 16:27:36 -0500

    Minute 40:28

---
+++ src/KotlinTut.kt
@@ -0,0 +1,192 @@
+package demo
+
+import java.util.Random
+
+fun main(args: Array<String>){
+//    var bigInt: Int = Int.MAX_VALUE
+//    var smallInt: Int = Int.MIN_VALUE
+//
+//    println("Biggest int : " + bigInt)
+//    println("Smallest int : $smallInt")
+
+    //Long Double Float Boolean Short Byte Char
+//
+//    var bigLong: Long = Long.MAX_VALUE
+//    var smallLong: Long = Long.MIN_VALUE
+//
+//    println("Biggest Long : " + bigLong)
+//    println("Smallest Long : " + smallLong)
+//
+//    var bigDouble: Double = Double.MAX_VALUE
+//    var smallDouble: Double = Double.MIN_VALUE
+//
+//    println("Biggest Double : " + bigDouble)
+//    println("Smallest Double : " + smallDouble)
+//
+//    var bigFloat: Float = Float.MAX_VALUE
+//    var smallFloat: Float = Float.MIN_VALUE
+//
+//    println("Biggest Float : " + bigFloat)
+//    println("Smallest Float : " + smallFloat)
+
+//    var dblNum1: Double = 1.1111111111111111
+//    var dblNum2: Double = 1.1111111111111111
+//
+//    println("Sum : " +(dblNum1 + dblNum2))
+//
+//    if (true is Boolean){
+//
+//        print("true is boolean\n")
+//    }
+//
+//    var letterGrade : Char = 'A'
+//
+//    println("A is a Char: ${letterGrade is Char}")
+//
+//    println("3.14 to Int : " + (3.14.toInt()))
+//
+//    println("A to Int : " + ('A'.toInt()))
+//
+//    println("65 to Char : " + (65.toChar()))
+
+//    val name = "Daniel Jimenez"
+//
+//    val longStr = """Daniel David Jimenez Paz
+//        
+//    Long String"""
+//
+//    var fName: String = "Doug"
+//
+//    var lName: String = "Smith"
+//
+//    fName = "Sally"
+//
+//    var fullName = fName + " " + lName
+//
+//    println("Name: $fullName")
+//
+//    println("1 + 2 = ${1+2}")
+//
+//    println("String Length :  = ${longStr.length}")
+//
+//    var str1 = "A random string"
+//    var str2 = "a random string"
+
+//    // Compare strings
+//    println("Strings Equal : ${str1.equals(str2)}")
+//
+//    // Compare strings
+//    // 0 : Equal, Negative if less, Positive if greater
+//    println("Compare A to B : ${"A".compareTo("B")}")
+
+//    println("2nd Index : ${str1[2]}")
+//
+//    println("Index 2 - 7 : ${str1.subSequence(2,8)}")
+//
+//    println("Contains random : ${str1.contains("random")}")
+//
+//    var myArray = arrayOf(1, 1.23, "Doug")
+//
+//    println(myArray[2])
+//
+//    myArray[1] = "What Ever"
+//
+//    println("ArrayLength : ${myArray.size}")
+//
+//    println("Doug in Array : ${myArray.contains("Doug")}")
+//
+//    var partArray = myArray.copyOfRange(0,1)
+//
+//    println("First : ${myArray.first()}")
+//
+//    println("Doug Index : ${myArray.indexOf("Doug")}")
+//
+//    var sqArray = Array(5, {x -> x*x})
+//
+//    println(sqArray[2])
+//
+//    var arr2: Array<Int> = arrayOf(1,2,3)
+
+//    val oneTo10 = 1..10
+//
+//    val alpha = "A".."Z"
+//
+//    println("R in alpha : ${"R" in alpha}")
+//
+//    val tenTo1 = 10.downTo(1)
+//
+//    val twoTo20 = 2.rangeTo(20)
+//
+//    val rng3 = oneTo10.step(3)
+//
+//    for(x in rng3) println("rng3 : $x")
+//
+//    for(x in tenTo1.reversed()) println("Reverse : $x")
+//
+//    val age = 8
+//    if (age < 5){
+//        println("Go to Preschool")
+//    } else if(age == 5){
+//        println("Go to Kinder Garden")
+//    }else if((age > 5) &&(age <= 17)){
+//
+//        val grade = age - 5
+//
+//        println("Go to Grade $grade")
+//    }else{
+//        println("Go to College")
+//    }
+//
+//    when(age){
+//        0,1,2,3,4 -> println("Go to Preschool")
+//        5-> println("Go to Kindergarden")
+//
+//        in 6..17 -> {
+//            val grade = age - 5
+//            println("Go to Grade $grade")
+//        }
+//        else -> println("Go to College")
+//    }
+//
+//    for(x in 1..10){
+//
+//        println("Loop : $x")
+//    }
+//
+//    val rand = Random()
+//
+//    val magicNum = rand.nextInt(50) + 1
+//
+//    var guess = 0
+//
+//    while(magicNum != guess){
+//        guess += 1
+//    }
+//     println("Magic Number was $guess")
+//
+//    for (x in 1..20){
+//
+//        if(x % 2 == 0){
+//            continue
+//        }
+//
+//        println("Odd : $x")
+//
+//        if(x == 15) break
+//    }
+//
+//    var arr3 : Array<Int> = arrayOf(3,6,9)
+//
+//    for(i in arr3.indices){
+//
+//        println("Mult 3 : ${arr3[i]}")
+//    }
+//
+//    for ((index,value) in arr3.withIndex()){
+//        println("Index: $index Value: $value")
+//    }
+
+    
+}
+
+
